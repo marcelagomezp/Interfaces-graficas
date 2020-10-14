ebe72ba3151127aab53811e9331dd8ffe6cad6f1
marcelagomezp <nicolle.gomez@mariaelvinia.edu.co>
2020-10-13 19:52:43 -0500

    Versión 2.0 Creación de la subclase ClubSocial con 3 atributos y 2 métodos

---
+++ Documents/NetBeansProjects/CasaDeEventos/src/casadeeventos/CasaDeEventos.java
@@ -0,0 +1,132 @@
+package casadeeventos;
+
+import java.util.Date;
+
+public class CasaDeEventos {
+
+    private String direccion; /**Lugar donde se encuentra el salón*/
+    private int cantidadDeEmpleados; /**Número de empleados disponibles*/
+    private Date fechasReservadas; /**Fechas en las cuales el salón de 
+     * eventos estará ocupado*/
+    private Boolean estaFuncionando; /**Se confirma si el salón está 
+     * disponible, al tener la cantidad requerida de empleados para 
+     * atender los eventos*/
+    private int aforoMáximo;/**Capacidad máxima de personas que el 
+     * salón puede recibir*/
+    
+    public CasaDeEventos(
+            String direccion, 
+            int cantidadDeEmpleados, 
+            Date fechasReservadas, 
+            Boolean estaFuncionando, 
+            int aforoMáximo) {
+        this.direccion = direccion;
+        this.cantidadDeEmpleados = cantidadDeEmpleados;
+        this.fechasReservadas = fechasReservadas;
+        this.estaFuncionando = estaFuncionando;
+        this.aforoMáximo = aforoMáximo;
+    }
+    
+    /**Este método tiene la función de cambiar 
+     * la dirección de la casa de eventos y aumentar en 20 la 
+     * capacidad de personas que puede recibir(aforo)
+     * @param nuevaDireccion es la nueva ubicación del salón
+     */
+        
+    public void cambiarDireccion(
+            String nuevaDireccion){
+        this.direccion = nuevaDireccion;
+        this.aforoMáximo += 20  ;
+    }
+    
+    /**Este metodo me permite cambiar la cantidad de empleados y
+     * confirmar si la Casa de eventos tiene los empleados suficientes
+     * para estar en funcionamiento, el cual debe ser un número mayor 
+     * o igual a 10.
+     * @param empleadosActuales es la nueva cantidad de empleados disponibles
+     * en el salón
+     */
+    
+    public void confirmarDisponibilidad(int empleadosActuales){
+        this.cantidadDeEmpleados = empleadosActuales;
+        if(empleadosActuales >= 10){
+         this.estaFuncionando = true;
+         }else{
+         this.estaFuncionando = false;}
+    }
+     
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getCantidadDeEmpleados() {
+        return cantidadDeEmpleados;
+    }
+
+    public void setCantidadDeEmpleados(int cantidadDeEmpleados) {
+        this.cantidadDeEmpleados = cantidadDeEmpleados;
+    }
+
+    public Date getFechasReservadas() {
+        return fechasReservadas;
+    }
+
+    public void setFechasReservadas(Date fechasReservadas) {
+        this.fechasReservadas = fechasReservadas;
+    }
+
+    public Boolean getEstaFuncionando() {
+        return estaFuncionando;
+    }
+
+    public void setEstaFuncionando(Boolean estaFuncionando) {
+        this.estaFuncionando = estaFuncionando;
+    }
+
+    public int getAforoMáximo() {
+        return aforoMáximo;
+    }
+
+    public void setAforoMáximo(int aforoMáximo) {
+        this.aforoMáximo = aforoMáximo;
+    }
+    
+    public static void main(String[] args) {
+        CasaDeEventos salon1 = new CasaDeEventos(
+                "Calle 20 #50-12",
+                24,
+                new Date(),
+                true,
+                30);
+        CasaDeEventos salon2 = new CasaDeEventos(
+                "Calle 20 #50-14",
+                90,
+                new Date(),
+                true,
+                110);
+        
+        
+        System.out.println("---Trabajo 1---");
+        System.out.println(salon1.getDireccion());
+        System.out.println(salon1.getAforoMáximo());
+        
+        salon1.cambiarDireccion("calle 50 # 4-22");//METODO 1
+        
+        System.out.println(salon1.getDireccion());
+        System.out.println(salon1.getAforoMáximo());
+        
+        System.out.println(salon2.getCantidadDeEmpleados());
+        System.out.println(salon2.getEstaFuncionando());
+        
+        salon2.confirmarDisponibilidad(9);//METODO 2
+        
+        System.out.println(salon2.getCantidadDeEmpleados());
+        System.out.println(salon2.getEstaFuncionando());
+        
+        
+    }
+}
---
+++ Documents/NetBeansProjects/CasaDeEventos/src/casadeeventos/ClubSocial.java
@@ -0,0 +1,88 @@
+package casadeeventos;
+
+import java.util.Date;
+
+public class ClubSocial extends CasaDeEventos{
+    private int numeroDePersonas; /**Es la cantidad de personas que se van 
+         a hospedar en el club*/
+    private boolean piscina; /**Pregunta al cliente si desea tener acceso a
+         las piscinas del club*/
+    private int diasReservados; /**Es la cantidad de días que se van a 
+         hospedar en el club*/
+
+    public ClubSocial(
+            String direccion, 
+            int cantidadDeEmpleados, 
+            Date fechasReservadas, 
+            Boolean estaFuncionando,
+            int aforoMáximo,
+            int numeroDePersonas,
+            boolean piscina,
+            int diasReservados) {
+        super(
+                direccion, 
+                cantidadDeEmpleados, 
+                fechasReservadas, 
+                estaFuncionando, 
+                aforoMáximo);
+        this.numeroDePersonas = numeroDePersonas;
+        this.piscina = piscina;
+        this.diasReservados = diasReservados;
+    }
+
+        public void accesoPiscina(){
+            if(this.numeroDePersonas >= 36){
+                this.piscina = false;
+                System.out.println("Lo sentimos, no podrá tener acceso a "
+                        + "la piscina, solo estará disponible para los clientes "
+                        + "que reserven para menos de 36 personas.");
+            }else{
+                this.piscina = true;
+                System.out.println("Tendrá acceso a la piscina");
+            }
+        }
+        
+        public void minimaMaximaEstadia(){
+            if(this.diasReservados <5){
+                System.out.println("El mínimo de días para reservar no puede "
+                        + "ser menor a 5"); 
+            }else if(this.diasReservados >15){
+                System.out.println("El máximo de dias para hospedarse en el "
+                        + "club es de 15");
+            }else{
+                System.out.println("Gracias por hacer su reservación");
+            }
+        }
+        
+        public static void main(String[] args) {
+            
+            System.out.println("---Club Social Stelar---");
+            
+            ClubSocial clubStelar = new ClubSocial(
+                    "Calle 22 #5-67", 
+                    15, 
+                    new Date(), 
+                    true, 
+                    12, 
+                    40, 
+                    true, 
+                    3);
+            clubStelar.minimaMaximaEstadia();
+            clubStelar.accesoPiscina();
+          
+            System.out.println("---Club Social Melanea---");
+            
+            ClubSocial clubMelanea = new ClubSocial(
+                    "Calle 35 #50-47", 
+                    36, 
+                    new Date(), 
+                    true, 
+                    80, 
+                    20, 
+                    true, 
+                    30);
+            clubMelanea.minimaMaximaEstadia();
+            clubMelanea.accesoPiscina();
+    } 
+       
+} 
